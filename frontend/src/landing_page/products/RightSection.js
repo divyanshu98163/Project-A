72061247c8045b95ce1d9a0b2060df24405e07a5
Divyanshu <divy9anshu@gmail.com>
2025-05-10 15:28:00 +0530

    new chages

--- frontend/src/landing_page/products/RightSection.js
+++ frontend/src/landing_page/products/RightSection.js
@@ -1,4 +1,4 @@
-<<<<<<< HEAD
+
 import React from "react";
 
 function RightSection({
@@ -40,46 +40,3 @@ function RightSection({
 }
 
 export default RightSection;
-=======
-import React from "react";
-
-function RightSection({
-  imageURL,
-  Productname,
-  productDescription,
-  learnMore,
-  googlePlay,
-  appStore,
-}) {
-  return (
-    <div className="container">
-      <div className="row">
-        
-        <div className="col-6 p-5 mt-5">
-          <h1>{Productname}</h1>
-          <p>{productDescription}</p>
-          <div>
-            <a href={learnMore} style={{ marginLeft: "50px" }}>
-              Learn more
-            </a>
-          </div>
-          <div className="mt-3">
-            <a href={googlePlay}>
-              <img src="media/images/googlePlayBadge.svg" />
-            </a>
-            <a href={appStore}>
-              <img src="media/images/appStoreBadge.svg" alt="" />
-            </a>
-          </div>
-        </div>
-        <div className="col-6 p-3">
-          <img src={imageURL} />
-        </div>
-      </div>
-      
-    </div>
-  );
-}
-
-export default RightSection;
->>>>>>> 82bf216ab9bc790b4bb5365a51dc89140e211d92
